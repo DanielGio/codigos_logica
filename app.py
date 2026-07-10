a35d8bf64898b93ee412df0edc99aa72737ca358
Daniel Giovane Florencio <florencedgio@gmail.com>
2026-07-10 20:24:19 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# Primeiro teste com variáveis
+
+name = "Joca da Silva"
+age = 35
+email = "jocadsilva@mail.com"
+birth_date = "10/08/1990"
+mother_name = "Maria"
+
+print (name)
+print (age)
+print (email)
+print (birth_date)
+print (mother_name)
+
