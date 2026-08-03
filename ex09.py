edc6549d400a83753e5b5dadd613da5d249db28a
Daniel Giovane Florencio <florencedgio@gmail.com>
2026-08-03 20:35:25 -0300

    Exercícios

---
+++ ex09.py
@@ -0,0 +1,8 @@
+# Questão (9) Pesquisa em uma lista (Operador in)
+
+nomes = ["Ana", "Carlos", "Marina", "Pedro", "Julia"]
+
+if "Pedro" in nomes:
+    print("Nome encontrado.")
+else:
+    print("Nome não encontrado.")
---
+++ ex10.py
@@ -0,0 +1,10 @@
+# Questão (10) Sistema de acesso completo
+
+idade = 20
+possui_convite = True
+
+if idade >= 18 and possui_convite:
+    print("Entrada autorizada.")
+else:
+    print("Entrada negada.")
+    
