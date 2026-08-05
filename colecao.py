46784da9f439cee69f0ca55ac5cb1de81f1ff1c0
Daniel Giovane Florencio <florencedgio@gmail.com>
2026-08-05 19:50:04 -0300

    Create colecao.py

---
+++ colecao.py
@@ -0,0 +1,27 @@
+roupas=[
+    {
+       "nome": "regata",
+        "codigo": "",
+        "preco": "19.99",
+        "quantidade": 5,
+        "categoria": "camisas"
+    },
+
+    {
+        "nome": "bermuda",
+        "codigo": 10,
+        "preco": "39.99",
+        "quantidade": 3,
+        "categoria": "bermudas"
+    },
+
+    {
+        "nome": "calca cargo",
+        "codigo": 15,
+        "preco": "59.99",
+        "quantidade": 2,
+        "categoria": "calcas"
+    },
+]
+
+print(roupas)
